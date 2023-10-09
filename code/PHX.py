89d7e8e33f92fc46963594391b6646fc4a71782d
JacobMcGill <jacobpmcgill97@gmail.com>
2023-10-08 19:50:13 -0500

    First pass at PHX data

--- code/PHX.py
+++ code/PHX.py
@@ -27,6 +27,26 @@ def remove_data(parking_dicts):
     return cut_data
 
 def transform_data(cut_data)
-"""Transforms RateTime values to cash value"""
+"""Transforms RateTime values to average rate in $ per hour"""
 for data in cut_data:
-    
+    rate = "Average Rate"
+    if data["RateTimeLimits"] = "$1.00/hr 1hr 8A-10P Mon-Fri" or "$1.00/hr 2hr 8A-10P Mon-Fri" or "$1.00/hr 2hr 8A-10P Mon-Fri" or "$1.00/hr 30min 8A-10P Mon-Fri" or "$1.00/hr 4hr 8A-10P Mon-Fri" or "$1.00/hr 8hr 8A-10P Mon-Fri":
+        data[rate] =  "$1.00"
+    if data["RateTimeLimits"] = "$1.50/hr 15min max 8A-10P Mon-Sun" or "$1.50/hr 15min max 8A-5P M-F" or "$1.50/hr 1hr 8A-5P, 4HR 5P-10P" or "$1.50/hr 1hr max 8A-10P Mon-Sun" or "$1.50/hr 1hr max 8A-5P M-F" or "$1.50/hr 1hr Max 8A-5P Mon-Friday, 2hr Max 5P-10P Mon-Friday 2hr Max Sat & Sun" or "$1.50/hr 1hr Max 8A-5P Mon-Friday, 2hr Max 5P-10P Mon-Friday 2hr Max Sat & Sun-ADA" or "$1.50/hr 2hr 8A-5P, 4HR 5P-10P" or "$1.50/hr 2hr max 8A-10P Mon-Sun" or "$1.50/hr 30min 8A-5P, 4HR 5P-10P" or "$1.50/hr 30min max 8A-10P Mon-Su" or "$1.50/hr 30min max 8A-5P M-F" or "$1.50/hr 4hr max 8A-10P Mon-Sun" or "$1.50/hr 4hr max 8A-10P Mon-Sun PBP" or "$1.50/hr 4hr max 8A-10P Mon-Sun-ADA" or "$1.50/hr 4hr max 8A-5P M-F" or "$1.50/hr 4hr max 8A-5P M-F-ADA" or "$1.50/hr 5hr max 8A-10P Mon-Sun" or "$1.50/hr 5hr max 8A-10P Mon-Sun-ADA" or "$1.50/hr 8hr max 8A-5P M-F" or "ASU $1.50/hr 1hr 8A-4P Mon-Thur, 8a-10p Fri-Sun & 6hr 4P-10P Mon-Thur" or "ASU $1.50/hr 2hr Max 8A-4P Mon-Thur, 8A-10P Fri-Sun & 6hr Max 4P-10P Mon-Thur":
+        data[rate] = "$1.50"
+    if data["RateTimeLimits"] =  "2hr" or "Copy of 8859 - M5+" or "Loading Zone 2hr Max" or "Loading Zone 4hr Max":
+        data[rate] = "$0.00"
+for data in cut_data:
+    del data["RateTimeLimits"]
+transform_data = cut_data
+return transform_data
+
+def proper_names(transform_data)
+"""Creates new keys with proper names for remaining data and adds city key-value"""
+    for data in transform_data:
+        data["City"] = "Phoenix"
+        if "StreetAddress" in data:
+            data["Street Address"] = data["StreetAddress"]
+            del data["StreetAddress"]
+    proper_data = transform_data
+    return transform_data
