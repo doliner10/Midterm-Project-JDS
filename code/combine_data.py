a30926907ed4ff2c5c045b6ffa92aae0a3993602
JacobMcGill <jacobpmcgill97@gmail.com>
2023-10-10 14:49:51 -0500

    Continued working on py to combine data and write CSV

--- code/combine_data.py
+++ code/combine_data.py
@@ -1,3 +1,26 @@
 import CSV
 import os
-from SJ
+from SJ import load_SJ_Parking and cut_data and tranform_data and SJ_data
+
+combined_data = list()
+SJ_data = load_SJ_parking()
+cut_data = remove_data(SJ_data)
+transform_data = transform_data(cut_data)
+SJ_data = rename_data(transform_data)
+
+#Need to add lists of dicts to add them together 
+
+def sort_data(combined_data)
+    """Sorts data by cities"""
+    city_sort = sorted(data, key=lambda x: x["City"])
+    final_data = city_sort
+    return final data
+
+def write_data_to_CSV(final_data)
+    """Writes data to a CSV"""
+    fieldname = ["City", "Street Address", "Rate"]
+    with open(path, 'w+') as file:
+        dict_writer = csv.DictWriter(f = file, fieldnames = fieldname)
+        dict_writer.writeheader()
+        dict_writer.writerows(data)
+    return
