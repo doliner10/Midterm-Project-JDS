c7a621365f823fca739dcdaa7374889eb8f66d77
doliner10 <doliner@utexas.edu>
2023-10-11 17:46:19 -0500

    writing indy to CSV

--- code/indianapolis.py
+++ code/indianapolis.py
@@ -5,7 +5,8 @@ import re
 INPUT_DIR = os.path.join("file")
 INDY_PATH = os.path.join(INPUT_DIR, "Indianapolis Parking Info.csv")
 OUTPUT_DIR = "artifacts"
-OUTPUT_PATH = os.path.join(OUTPUT_DIR, "Indianapolis")
+OUTPUT_FILENAME = "Indianapolis.csv"
+OUTPUT_PATH = os.path.join(OUTPUT_DIR, OUTPUT_FILENAME)
 
 def indy_parking_data():
     parking_dicts = list()
@@ -15,7 +16,7 @@ def indy_parking_data():
         parking_dicts.append(row)
     return parking_dicts
 
-def narrow_down_data(parking_dicts):
+def indy_narrow_down_data(parking_dicts):
     # Define the columns to keep (relevant columns)
     relevant_columns = ["FULL_ADDRESS", "HOURLY_RATE"]  
     
@@ -26,10 +27,10 @@ def narrow_down_data(parking_dicts):
     
     return relevant_data
 
-def transform_data(relevant_data):
+def indy_transform_data(relevant_data):
     "Transforming Rate Time data into average rates"
     for cut_dict in relevant_data:
-        rate = "Average Rate"
+        rate = "Rate"
         if cut_dict["HOURLY_RATE"] == "1.5" or "1.50":
             cut_dict[rate] =  "1.50"
         if cut_dict["HOURLY_RATE"] == "1" or "1.0":
@@ -39,7 +40,7 @@ def transform_data(relevant_data):
     transform_data = relevant_data
     return transform_data
 
-def proper_names(transform_data):
+def indy_proper_names(transform_data):
     """Creates new keys with proper names for remaining data and adds city key-value"""
     for data in transform_data:
         data["City"] = "Indianapolis"
@@ -50,8 +51,17 @@ def proper_names(transform_data):
 
 if __name__ == "__main__":
     parking_data = indy_parking_data()
-    narrowed_data = narrow_down_data(parking_data)
-    transformed_data = transform_data(narrowed_data)
-    modified_data = proper_names(transformed_data)
+    narrowed_data = indy_narrow_down_data(parking_data)
+    transformed_data = indy_transform_data(narrowed_data)
+    modified_data = indy_proper_names(transformed_data)
 
     print(modified_data)
+ 
+    new_csv_path = OUTPUT_PATH
+
+    fieldnames = ["City", "Street Address", "Rate"]
+# Write the modified_data to a new CSV file
+    with open(new_csv_path, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(modified_data) 
