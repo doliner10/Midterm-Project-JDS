6143dd15ddf1b9ba100dd5061bd16aef06fce8a2
JacobMcGill <jacobpmcgill97@gmail.com>
2023-10-11 15:07:49 -0500

    Worked on combine data

--- code/combine_data.py
+++ code/combine_data.py
@@ -13,7 +13,11 @@ cut_data = remove_data(SJ_data)
 transform_data = transform_data(cut_data)
 SJ_data = rename_data(transform_data)
 
-#Need to add lists of dicts to add them together 
+
+def combine_data()
+combined_data = SJ_data + SF_data + PHX_data + SD_data + Charlotte_data + NYC_data + Indianapolis_data + LA_data 
+
+
 
 def sort_data(combined_data)
     """Sorts data by cities"""
@@ -29,3 +33,8 @@ def write_data_to_CSV(final_data, path)
         dict_writer.writeheader()
         dict_writer.writerows(data)
     return
+
+if __name__ == "__main__":
+    combined_data = SJ_data + SF_data + PHX_data + SD_data + Charlotte_data + NYC_data + Indianapolis_data + LA_data
+    final_data = sort_data(combined_data)
+    write_data_to_CSV(fina_data, CSV_PATH)
