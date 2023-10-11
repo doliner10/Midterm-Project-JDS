88ab246881c21124737515ffb7d9bae60a8c7680
JacobMcGill <jacobpmcgill97@gmail.com>
2023-10-11 15:28:52 -0500

    completed combined data

--- code/combine_data.py
+++ code/combine_data.py
@@ -6,6 +6,8 @@ from SF import load_SF_Parking and cut_data and tranform_data and rename_data
 from PHX import load_PHX_Parking and cut_data and tranform_data and rename_data
 from nyc import load_NYC_parking and remove_data and calculate_average
 from charlotte import load_charlotte_parking and remove_data
+from indianapolis import indy_parking_data and narrow_down_data and transform_data and proper_names
+from la import LA_parking_data and narrow_down_data and transform_data and convert_average_rate_to_float and proper_names
 
 BASE_DIR = "artifacts"
 CSV_PATH = os.path.join(BASE_DIR, "results.csv")
@@ -51,8 +53,17 @@ def load_Charlotte()
     Charlotee_data = remove_data(Charlotte_data)
 def load_Indianapolis()
     """Loads Indianapolis data"""
-    Indianapolis_data = 
-
+    parking_data = indy_parking_data()
+    narrowed_data = narrow_down_data(parking_data)
+    transformed_data = transform_data(narrowed_data)
+    Indianapolis_data = proper_names(transformed_data)
+def load_LA()
+    """Loads in LA data"""
+    parking_data = LA_parking_data()
+    narrowed_data = narrow_down_data(parking_data)
+    transformed_data = transform_data(narrowed_data)
+    converted_data = convert_average_rate_to_float(transformed_data)
+    LA_data = proper_names(converted_data)
 
 
 def sort_data(combined_data)
@@ -71,6 +82,14 @@ def write_data_to_CSV(final_data, path)
     return
 
 if __name__ == "__main__":
+    SJ_data = load_SJ()
+    PHX_data = load_PHX()
+    SF_data = load_SF()
+    SD_data = load_SD()
+    NYC_data = load_nyc()
+    Charlotte_data = load_Charlotte()
+    Indianapolis_data = load_Indianapolis()
+    LA_data = load_LA()
     combined_data = SJ_data + SF_data + PHX_data + SD_data + Charlotte_data + NYC_data + Indianapolis_data + LA_data
     final_data = sort_data(combined_data)
-    write_data_to_CSV(fina_data, CSV_PATH)
+    write_data_to_CSV(final_data, CSV_PATH)
