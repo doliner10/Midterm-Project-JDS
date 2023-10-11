859b3b0e7856a4ce1c43cfc71524cb4b6b53bf2f
JacobMcGill <jacobpmcgill97@gmail.com>
2023-10-11 17:58:41 -0500

    Figured out PHX CSV

--- code/PHX.py
+++ code/PHX.py
@@ -40,15 +40,12 @@ def transform_data(cut_data):
     """Transforms RateTime values to average rate in $ per hour and returns the most common max time limit for each spot"""
     for cut_dict in cut_data:
         rate = "Rate"
-        if "$1.00" in cut_dict["RateTimeLimits"]: #== "$1.00/hr 1hr 8A-10P Mon-Fri" or "$1.00/hr 2hr 8A-10P Mon-Fri" or "$1.00/hr 2hr 8A-10P Mon-Fri" or "$1.00/hr 30min 8A-10P Mon-Fri" or "$1.00/hr 4hr 8A-10P Mon-Fri" or "$1.00/hr 8hr 8A-10P Mon-Fri":
-            cut_dict[rate] =  float(1.00)
-        if cut_dict["RateTimeLimits"] == "$1.50/hr 15min max 8A-10P Mon-Sun" or "$1.50/hr 15min max 8A-5P M-F" or "$1.50/hr 1hr 8A-5P, 4HR 5P-10P" or "$1.50/hr 1hr max 8A-10P Mon-Sun" or "$1.50/hr 1hr max 8A-5P M-F" or "$1.50/hr 1hr Max 8A-5P Mon-Friday, 2hr Max 5P-10P Mon-Friday 2hr Max Sat & Sun" or "$1.50/hr 1hr Max 8A-5P Mon-Friday, 2hr Max 5P-10P Mon-Friday 2hr Max Sat & Sun-ADA" or "$1.50/hr 2hr 8A-5P, 4HR 5P-10P" or "$1.50/hr 2hr max 8A-10P Mon-Sun" or "$1.50/hr 30min 8A-5P, 4HR 5P-10P" or "$1.50/hr 30min max 8A-10P Mon-Su" or "$1.50/hr 30min max 8A-5P M-F" or "$1.50/hr 4hr max 8A-10P Mon-Sun" or "$1.50/hr 4hr max 8A-10P Mon-Sun PBP" or "$1.50/hr 4hr max 8A-10P Mon-Sun-ADA" or "$1.50/hr 4hr max 8A-5P M-F" or "$1.50/hr 4hr max 8A-5P M-F-ADA" or "$1.50/hr 5hr max 8A-10P Mon-Sun" or "$1.50/hr 5hr max 8A-10P Mon-Sun-ADA" or "$1.50/hr 8hr max 8A-5P M-F" or "ASU $1.50/hr 1hr 8A-4P Mon-Thur, 8a-10p Fri-Sun & 6hr 4P-10P Mon-Thur" or "ASU $1.50/hr 2hr Max 8A-4P Mon-Thur, 8A-10P Fri-Sun & 6hr Max 4P-10P Mon-Thur":
-            cut_dict[rate] = float(1.50)
-        if cut_dict["RateTimeLimits"] ==  "2hr" or "Copy of 8859 - M5+" or "Loading Zone 2hr Max" or "Loading Zone 4hr Max":
+        if "$" not in cut_dict["RateTimeLimits"]: 
             cut_dict[rate] = float(0.00)
-        # if cut_dict["RateTimeLimits"] ==  "":
-        #     cut_dict[rate] = float(0.00)
-
+        if "$1.00/hr" in cut_dict["RateTimeLimits"]: 
+            cut_dict[rate] =  float(1.00)
+        if "$1.50/hr" in cut_dict["RateTimeLimits"]: 
+            cut_dict[rate] = float(1.50)
     for cut_dict in cut_data:
         del cut_dict["RateTimeLimits"]
     transform_data = cut_data
